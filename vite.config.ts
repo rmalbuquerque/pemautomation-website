9cb6a1ea9947dd5fd86c33580d6c94a25720594e
Ricardo Albuquerque <79746801+rmalbuquerque@users.noreply.github.com>
2025-11-09 16:14:16 +0000

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Use VITE_BASE para definir dinamicamente a base antes do build.
-// Por exemplo: VITE_BASE=/pemautomation-website/ pnpm run build
-export default defineConfig(() => ({
-  base: process.env.VITE_BASE || '/pemautomation-website/',
-  plugins: [react()],
-}))
